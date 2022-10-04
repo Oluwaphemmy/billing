8dace92ce87e7d0fcffa28af744f021909d82568
Oluwaphemmy <ojokofemi@gmail.com>
2022-10-04 15:32:06 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        body: "#E8E8FD",
+        pri: "#f8f8ff",
+        gasper: "#1a194d",
+      },
+    },
+  },
+  plugins: [],
+};
